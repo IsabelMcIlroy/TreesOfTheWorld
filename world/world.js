825cdc4c6fd8885f09ed6dfe64d2dd6890f3ce26
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-09-21 11:45:19 -0230

    tree names

--- world/world.js
+++ world/world.js
@@ -2,8 +2,10 @@ const exampleModal = document.getElementById("exampleModal");
 exampleModal.addEventListener("show.bs.modal", function (event) {
   const button = event.relatedTarget;
   const country = button.getAttribute("data-bs-country");
+  const tree = button.getAttribute("data-bs-tree");
   const modalTitle = exampleModal.querySelector(".modal-title-country");
-  //const modalBody = exampleModal.querySelector('.modal-body country')
+  const modalTreeName = exampleModal.querySelector(".modal-tree-name");
 
   modalTitle.textContent = country + " National Tree";
+  modalTreeName.textContent = tree;
 });
