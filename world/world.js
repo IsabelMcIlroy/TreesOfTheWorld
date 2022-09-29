e0d31015095073592ae5c7cfa7721938c3edddbc
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-09-29 14:20:10 -0230

    img source

--- world/world.js
+++ world/world.js
@@ -1,13 +1,23 @@
 const exampleModal = document.getElementById("exampleModal");
-exampleModal.addEventListener("show.bs.modal", function (event) {
+exampleModal.addEventListener("show.bs.modal", (event) => {
   const button = event.relatedTarget;
   const country = button.getAttribute("data-bs-country");
   const tree = button.getAttribute("data-bs-tree");
   const modalTitle = exampleModal.querySelector(".modal-title-country");
   const modalTreeName = exampleModal.querySelector(".modal-tree-name");
   const modalTreeImage = exampleModal.querySelector(".modal-tree-image");
+  const modalTreeSource = exampleModal.querySelector(".modal-tree-source");
 
   modalTitle.textContent = country + " National Tree";
   modalTreeName.textContent = tree;
   modalTreeImage.src = "./trees/" + tree + ".jpg";
+  getModalSource(modalTreeSource, modalTreeName);
 });
+
+getModalSource = (modalTreeSource, modalTreeName) => {
+  if (modalTreeName.textContent === "Maple (Acer)") {
+    modalTreeSource.textContent = "maple tree";
+  } else {
+    modalTreeSource.textContent = "ops";
+  }
+};
