5e35d3d1f34596be0a42847fde49c6990f37e824
Isabel McIlroy <isabelcohlmeyer@outlook.com>
2022-10-06 11:48:47 -0230

    tree pictures done!
    now to add descriptions to them all!

--- world/world.js
+++ world/world.js
@@ -183,7 +183,7 @@ getModalSource = (modalTreeSource, modalTreeName) => {
     modalTreeSource.textContent =
       "By Luisalvaz - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=39290623";
   } else if (
-    (modalTreeName.textContent = "Borneo/Malacca Teak (Intsia Palembanica)")
+    (modalTreeName.textContent = "Borneo or Malacca Teak (Intsia Palembanica)")
   ) {
     modalTreeSource.textContent =
       "By filibot.web - https://www.flickr.com/photos/filibot/8003919056, CC BY-SA 2.0, https://commons.wikimedia.org/w/index.php?curid=82148719";
@@ -198,6 +198,83 @@ getModalSource = (modalTreeSource, modalTreeName) => {
   } else if ((modalTreeName.textContent = "Silver Fern (Cyathea Dealbata)")) {
     modalTreeSource.textContent =
       "By Tatania Gerus - https://www.flickr.com/photos/tgerus/2349086151/in/set-72157604396543621, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=4145474";
+  } else if ((modalTreeName.textContent = "Panama Tree (Sterculia Apetala)")) {
+    modalTreeSource.textContent =
+      "By Christian Pirkl - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=68367635";
+  } else if ((modalTreeName.textContent = "Cinchona Officinalis (Cinchona)")) {
+    modalTreeSource.textContent =
+      "By Bentham-Moxon Trust.; Curtis, William; Curtis&#039;s botanical magazine dedications, 1827-1927: portraits and biographical notes.; Royal Botanic Gardens, Kew.; Stanley Smith Horticultural Trust. - https://www.flickr.com/photos/biodivlibrary/8272601122, Public Domain, https://commons.wikimedia.org/w/index.php?curid=42754692";
+  } else if ((modalTreeName.textContent = "Narra (Pterocarpus Indicus)")) {
+    modalTreeSource.textContent =
+      "By Geographer - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=116052040";
+  } else if ((modalTreeName.textContent = "Deodar (Cedrus Deodara)")) {
+    modalTreeSource.textContent =
+      "By Paul Evans from London, United Kingdom - DSC00483.JPG, CC BY 2.0, https://commons.wikimedia.org/w/index.php?curid=9471983";
+  } else if ((modalTreeName.textContent = "Cork oak (Quercus Suber)")) {
+    modalTreeSource.textContent =
+      "By Kent Wang from London, United Kingdom - Cork oak, CC BY-SA 2.0, https://commons.wikimedia.org/w/index.php?curid=73644853";
+  } else if (
+    (modalTreeName.textContent = "Lapacho (Handroanthus Impetiginosus)")
+  ) {
+    modalTreeSource.textContent =
+      "By © 2006 Carla Antonini - Autoría propia., CC BY-SA 2.5 ar, https://commons.wikimedia.org/w/index.php?curid=1065154";
+  } else if (
+    (modalTreeName.textContent =
+      "Christ's Thorn Jujube (Ziziphus Spinachristi)")
+  ) {
+    modalTreeSource.textContent =
+      "By Meincluded - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=76058112";
+  } else if ((modalTreeName.textContent = "Siberian Larch (Larix Sibricia)")) {
+    modalTreeSource.textContent =
+      "By Montréalais - No machine-readable source provided. Own work assumed (based on copyright claims).en.wikipedia.org, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=417632";
+  } else if (
+    (modalTreeName.textContent = "Phoenix Palm (Phoenix Dactylifera)")
+  ) {
+    modalTreeSource.textContent =
+      "By Mmcknight4 - Own work, Public Domain, https://commons.wikimedia.org/w/index.php?curid=3717908";
+  } else if (
+    (modalTreeName.textContent = "Ornas Birch (Betual Pendula 'Dalecarlica')")
+  ) {
+    modalTreeSource.textContent =
+      "By This image was produced by me, David Castor (user:dcastor) Own work, CC0, https://commons.wikimedia.org/w/index.php?curid=16363970";
+  } else if ((modalTreeName.textContent = "Linden (Tilia)")) {
+    modalTreeSource.textContent =
+      "By Bruce Marlin - Own work: http://www.cirrusimage.com/tree_silver_linden_upright.htm, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=5742109";
+  } else if ((modalTreeName.textContent = "Maquilishuat (Tabebuia Rosea)")) {
+    modalTreeSource.textContent =
+      "By José David Villalobos Robles - Own work, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=2461295";
+  } else if ((modalTreeName.textContent = "Ratchaphruek (Cassia Fistula)")) {
+    modalTreeSource.textContent =
+      "By Banswalhemant - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=90721202";
+  } else if (
+    (modalTreeName.textContent = "African Blackwood (Dalbergia Melanoxylon)")
+  ) {
+    modalTreeSource.textContent =
+      "By Mogano at German Wikipedia, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=15864532";
+  } else if ((modalTreeName.textContent = "Williow (Salix)")) {
+    modalTreeSource.textContent =
+      "By Bruce Marlin - Own work: http://www.cirrusimage.com/tree_golden_weeping_willow.htm, CC BY 3.0, https://commons.wikimedia.org/w/index.php?curid=5726820";
+  } else if (
+    (modalTreeName.textContent = "Arbol de Artigas (eltphorum Dubium)")
+  ) {
+    modalTreeSource.textContent =
+      "By אבישי טייכר - Own work, Public Domain, https://commons.wikimedia.org/w/index.php?curid=2400587";
+  } else if ((modalTreeName.textContent = "Araguaney (Tabeluia Chrysantha)")) {
+    modalTreeSource.textContent =
+      "By José Reynaldo da Fonseca - Own work, CC BY 2.5, https://commons.wikimedia.org/w/index.php?curid=1161568";
+  } else if ((modalTreeName.textContent = "Lotus (Nelumbo Nucifera)")) {
+    modalTreeSource.textContent =
+      "By T.Voekler - Own work, CC BY-SA 3.0, https://commons.wikimedia.org/w/index.php?curid=8142220";
+  } else if (
+    (modalTreeName.textContent = "Dragon Blood Tree (Dracaena Cinnabari)")
+  ) {
+    modalTreeSource.textContent =
+      "By Boris Khvostichenko(User:Boriskhv) - Own work, CC BY-SA 4.0, https://commons.wikimedia.org/w/index.php?curid=7175533";
+  } else if (
+    (modalTreeName.textContent = "Real Yellowwood (Podocarpus Latifolius)")
+  ) {
+    modalTreeSource.textContent =
+      "By Abu Shawka - Own workTransferred from en.wikipedia, Public Domain, https://commons.wikimedia.org/w/index.php?curid=12527782";
   } else {
     modalTreeSource.textContent = "ops";
   }
